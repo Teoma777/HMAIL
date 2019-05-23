2630da558bf4d0dca405fb200738fdbca11c5db3
Teoma777 <groux.thomas.uniscite@gmail.com>
2019-05-23 16:10:24 +0200

    2.4 ok

--- app/controllers/emails_controller.rb
+++ app/controllers/emails_controller.rb
@@ -2,26 +2,23 @@ class EmailsController < ApplicationController
 	require 'faker'
   def index
   	@emails = Email.all
-
-
-
-
-
-
-
-
-
-
-
   	 end
+
   def new
 
   end
 
-
-
-def create
+  def create
 	    respond_to do |format|
+      format.html {  }
+      format.js { }
+	end
+end
+
+def show
+	@current_email = Email.find(params[:id])
+	@current_email_id = @current_email.id
+		    respond_to do |format|
       format.html {  }
       format.js { }
 end
